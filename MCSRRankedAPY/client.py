825a5719b32bcd0305729e2cc04d8aea17714bbf
Myer <19848162+xiaomyer@users.noreply.github.com>
2025-05-08 13:16:46 -0400

    implement leaderboard endpoints

--- MCSRRankedAPY/client.py
+++ MCSRRankedAPY/client.py
@@ -2,6 +2,7 @@ from aiohttp import ClientSession
 from objects.match_info import MatchInfo
 from objects.user_profile import UserProfile
 from objects.versus_stats import VersusStats
+from objects.leaderboards import EloLeaderboard, SeasonPhasePointsLeaderboard, BestTimeEntry, WeeklyRaceLeaderboard
 from typing import Optional
 from objects.exceptions import *
 
@@ -12,7 +13,7 @@ class ClientSessionWrapper:
   def __init__(self, base_url: str):
     self._client = ClientSession(base_url=base_url)
 
-  async def request_json(self, method: str, path: str, params: dict) -> dict:
+  async def request_json(self, method: str, path: str, params: dict = {}) -> dict:
     async with self._client.request(method, path, params=params) as response:
       if response.status == 400:
         raise DataNotFoundException(path, params)
@@ -37,6 +38,8 @@ class MCSRRankedClient:
     self.key = key
     self.users = Users(self._client)
     self.matches = Matches(self._client)
+    self.leaderboards = Leaderboards(self._client)
+    self.weekly_races = WeeklyRaces(self._client)
 
   async def close(self):
     await self._client.close()
@@ -95,3 +98,39 @@ class Matches:
     json = await self._client.request_json("GET", f"matches/{id}", params=kwargs)
     data = json.get("data")
     return MatchInfo(**data)
+
+
+class Leaderboards:
+  def __init__(self, client: ClientSessionWrapper, key: str = None):
+    self._client = client
+    self.key = key
+
+  async def get_elo(self, **kwargs):
+    """Gets the ELO leaderboard (top 150 but not always 150)"""
+    json = await self._client.request_json("GET", "leaderboard", params=kwargs)
+    data = json.get("data")
+    return EloLeaderboard(**data)
+
+  async def get_season_phase_points(self, **kwargs):
+    """Gets the season phase points leaderboard"""
+    json = await self._client.request_json("GET", "phase-leaderboard", params=kwargs)
+    data = json.get("data")
+    return SeasonPhasePointsLeaderboard(**data)
+
+  async def get_season_phase_points(self, **kwargs):
+    """Gets the season best time leaderboard"""
+    json = await self._client.request_json("GET", "record-leaderboard", params=kwargs)
+    data = json.get("data")
+    return [BestTimeEntry(**data[i]) for i in range(len(data))]
+
+
+class WeeklyRaces:
+  def __init__(self, client: ClientSessionWrapper, key: str = None):
+    self._client = client
+    self.key = key
+
+  async def get_info_leaderboard(self, id: int = -1, **kwargs):
+    """Get weekly race info and leaderboard"""
+    json = await self._client.request_json("GET", f"weekly-race{'' if id < 0 else f'/{id}'}")
+    data = json.get("data")
+    return WeeklyRaceLeaderboard(**data)
---
+++ MCSRRankedAPY/objects/leaderboards.py
@@ -0,0 +1,58 @@
+from pydantic import BaseModel, Field
+from datetime import datetime, timedelta
+from typing import List
+from objects.user_profile import UserProfile
+
+
+class Season(BaseModel):
+  endsAt: datetime
+  number: int
+
+
+class EloLeaderboard(BaseModel):
+  season: Season
+  users: List[UserProfile] = Field(default_factory=list)
+
+
+class Phase(BaseModel):
+  endsAt: datetime
+  number: int
+  season: int
+
+
+class SeasonPhasePointsLeaderboard(BaseModel):
+  phase: Phase
+  users: List[UserProfile] = Field(default_factory=list)
+
+
+class BestTimeEntry(BaseModel):
+  rank: int
+  season: int
+  date: datetime
+  id: int
+  time: timedelta
+  user: UserProfile
+
+
+# class BestTimeLeaderboard(BaseModel):
+
+
+class WeeklyRaceEntry(BaseModel):
+  rank: int
+  player: UserProfile
+  time: timedelta
+  replayExist: bool
+
+
+class WeeklyRaceSeed(BaseModel):
+  overworld: str
+  nether: str
+  theEnd: str
+  rng: str
+
+
+class WeeklyRaceLeaderboard(BaseModel):
+  id: int
+  seed: WeeklyRaceSeed
+  endsAt: datetime
+  leaderboard: List[WeeklyRaceEntry] = Field(default_factory=list)
